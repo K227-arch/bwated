96d95494bb7819a8bf700d6cd87972399811a50f
Keith <Keith@gmail.com>
2025-03-02 15:35:26 +0300

    keith2

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -102,7 +102,10 @@ function App({hideSideNav, isSideNavVisible}) {
 
       <div className="chat-container2-dashboard">
         <header className="chat-header">
-          <h1>Good morning, {user?.user_metadata?.full_name || 'User'}</h1>
+            {/*<div className="morning">
+            <h1>Good morning, {user?.user_metadata?.full_name || 'User'}</h1>
+          </div>*/}
+          
           
           <div className="container4">
                 <div className="layout-main">
